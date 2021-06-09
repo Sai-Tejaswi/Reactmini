9bb0dc67eebfea3c9c7f3bd8aac93e3ea9b51f7a
Sai-Tejaswi <chisate1998@gmail.com>
2021-06-09 17:59:24 +0530

    created elements using jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+     <h1>NETFLIX pick</h1>
+     <p>There are 5 best series</p>
+     <ol>
+       <li>DARK</li>
+       <li>My HOLO LOVe</li>
+       <li>extra circular</li>
+       <li>hello love</li>
+     </ol>
+
     </div>
   );
 }
